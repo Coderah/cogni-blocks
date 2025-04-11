5a2ea8c5f7ea02d2d1774f7e6ca6df0a2dedbd42
Alex Howard <alex@howardsupply.com>
2025-04-11 16:14:26 -0700

    boilerplate; runnable client-side

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue';
+import './style.css';
+import App from './app.vue';
+
+const app = createApp(App);
+
+app.mount('#app');
+
+if (import.meta.hot) {
+    import.meta.hot.accept();
+}
